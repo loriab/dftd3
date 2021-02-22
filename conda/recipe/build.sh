fddf5df7d9089ffa7005aff340b1f446d99266be
Lori A. Burns <lori.burns@gmail.com>
2021-02-22 16:32:49 -0500

    t59

--- conda/recipe/build.sh
+++ conda/recipe/build.sh
@@ -23,7 +23,9 @@ if [ "$(uname)" == "Linux" ]; then
 
     # load Intel compilers
     set +x
-    source /opt/intel/oneapi/setvars.sh intel64
+#    source /opt/intel/oneapi/setvars.sh intel64
+    LATEST_VERSION=$(ls -1 /opt/intel/oneapi/compiler/ | grep -v latest | sort | tail -1)
+    source /opt/intel/oneapi/compiler/"$LATEST_VERSION"/env/vars.sh intel64
     set -x
 
     # link against conda GCC
